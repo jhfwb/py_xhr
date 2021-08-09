00a3dbbc9d41593b5862d7dd364c1fc79a5353a1
xmind <527077832@qq.com>
2021-08-09 13:55:42 +0800

    修改version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 # sys.argv=['setup.py','sdist','bdist_wheel'] #将sys.argv的外部参数改成setup.py。相当于是运行python
 setuptools.setup(
     name="xhr",#软件包名称
-    version='0.0.0.0',
+    version='1.0.0.0',
     author="许焕燃",
     author_email='527077832@qq.com',
     description="暂无软件简介信息",
